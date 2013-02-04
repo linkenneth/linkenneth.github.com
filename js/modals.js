5a21bbad84c7ea75ae92bee70d181e25d5277d45
Kenneth Lin <kenlin@berkeley.edu>
2013-02-03 17:44:36 -0800

    added dates to each project

--- js/modals.js
+++ js/modals.js
@@ -7,6 +7,7 @@ modals = {
   karaoke: {
     id: "karaoke",
     name: "CSUA Hackathon - Karaoke Song Voter",
+    date: "February 2012",
     link: "https://github.com/linkenneth/Karaoke-Song-Voter",
     image: "img/karaoke-song-voter.jpg",
     quote: "Sing your hearts out"
@@ -15,6 +16,7 @@ modals = {
   scheme: {
     id: "scheme",
     name: "Scheme Interpreter",
+    date: "April 2012",
     link: "https://github.com/linkenneth/furry-bear",
     image: "img/furry-bear-scm.png",
     quote: "Scheme interpreter"
@@ -23,6 +25,7 @@ modals = {
   thunder_ant: {
     id: "thunder_ant",
     name: "Big Hack (Stanford vs. Cal) - Thunder-Ant",
+    date: "April 2012",
     link: "https://github.com/linkenneth/bighack",
     image: "img/thunder-ant-sharpened.png",
     quote: "Meshing CS and art"
@@ -31,6 +34,7 @@ modals = {
   chain_reaction: {
     id: "chain_reaction",
     name: "Chain Reaction Hackathon - More is More",
+    date: "November 2012",
     link: "https://github.com/linkenneth/chain-reaction",
     image: "img/chain-reaction.jpg",
     quote: "First place - Chain Reaction Hackathon."
@@ -39,6 +43,7 @@ modals = {
   mind_scorpion: {
     id: "mind_scorpion",
     name: "H@B-IEEE Hackathon - MindScorpion RC Cars",
+    date: "November 2012",
     link: "https://github.com/linkenneth/neurosky",
     image: "img/mind-scorpion.jpg",
     quote: "Best Hardware Hack"
@@ -47,6 +52,7 @@ modals = {
   lines_of_action: {
     id: "lines_of_action",
     name: "CS61B - Lines of Action",
+    date: "December 2012",
     link: "https://github.com/linkenneth/loa",
     image: "img/lines-of-action.jpg",
     quote: "CS61B final project"
@@ -55,6 +61,7 @@ modals = {
   benchmark: {
     id: "benchmark",
     name: "Benchmark Suite",
+    date: "December 2012",
     link: "https://github.com/linkenneth/benchmark",
     image: "img/benchmark.png",
     quote: "Benchmarking various algorithms on different languages"
@@ -63,10 +70,11 @@ modals = {
   eeg: {
     id: "eeg",
     name: "EE40 - Electroencephalogram",
+    date: "December 2012",
     link: "https://github.com/linkenneth/electroencephalography",
     image: "img/eeg1.jpg",
     quote: "Filtering for EEGs"
-  },
+  }
 
 }
 
